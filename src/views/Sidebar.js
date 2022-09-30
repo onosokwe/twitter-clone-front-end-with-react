1db85d641d1572726e6fd7ca4a209090ba94c41f
Andrew <57761772+onosokwe@users.noreply.github.com>
2022-09-30 10:03:51 +0100

    Update Sidebar.js

--- src/views/Sidebar.js
+++ src/views/Sidebar.js
@@ -14,14 +14,12 @@ const Sidebar = () => {
 				</div>
 				<div className="develop">
 					<ul>
-						<li>
-							<Link to="/home"><i className="fa fa-home"></i> <span>Home</span></Link>
-						</li>
+						<li><Link to=""><i className="fa fa-home"></i> <span>Home</span></Link></li>
 						<li><Link to=""><i className="fa fa-bell"></i> <span> Notifications</span></Link></li>
 						<li><Link to=""><i className="fa fa-envelope-o"></i><span>  Messages</span></Link></li>
 						<li><Link to=""><i className="fa fa-comment-o"></i><span>  Chat</span></Link></li>
 						<li><Link to=""><i className="fa fa-bookmark-o"></i> <span> Bookmarks</span></Link></li>
-						<li><Link to="/profile"><i className="fa fa-user"></i><span>  Profile</span></Link></li>
+						<li><Link to=""><i className="fa fa-user"></i><span>  Profile</span></Link></li>
 					</ul>
 				</div>
 			</aside>
